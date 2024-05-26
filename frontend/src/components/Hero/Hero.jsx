162ba87ec92639a353e31dac3b0229040afc3932
0x902 <ui.yazir@gmail.com>
2024-05-26 22:43:34 +0530

    Improved mobile responsiveness.

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -7,7 +7,12 @@ function Hero() {
             <p className="hero-description">
             Computer Science student, coding enthusiast, and minimalist. I write about technology, design, and coffee musings. Based in Sri Lanka.
             </p>
-            <a className="btn-contact" href="mailto:ui.yazir@gmail.com">Contact me &rarr;</a>
+            <div className="hero-actions">
+                <a title="Github URL" target="_blank" className="github-cta" href="https://www.github.com/0x902">
+                    <i className="devicon-github-original"></i>
+                </a>
+                <a className="btn-contact" href="mailto:ui.yazir@gmail.com">Contact me &rarr;</a>
+            </div>
         </div>
     )
 }
